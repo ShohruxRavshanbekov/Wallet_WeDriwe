2983b65fbc0dd65fa74e1e5a965604ef8cb77442
ShohruxRavshanbekov <shohrux.ravshanbekov001@gmail.com>
2025-04-26 09:31:20 +0500

    add new card

--- features/cards/src/main/java/uz/futuresoft/features/cards/components/CardData.kt
+++ features/cards/src/main/java/uz/futuresoft/features/cards/components/CardData.kt
@@ -1,53 +1,35 @@
 package uz.futuresoft.features.cards.components
 
-import android.content.Context
-import android.view.inputmethod.InputMethodManager
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import uz.futuresoft.common.R
 import uz.futuresoft.common.ui.theme.CardDataTextColor
 import uz.futuresoft.common.ui.theme.CardDataTextContainerBorderColor
 import uz.futuresoft.common.ui.theme.WalletWeDriveTheme
@@ -63,6 +45,9 @@ internal fun CardData(
     onCardNumberFieldFocused: () -> Unit,
     onCardExpireDateFieldFocused: () -> Unit,
 ) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     OutlinedCard(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(16.dp),
@@ -79,7 +64,17 @@ internal fun CardData(
             CustomOutlinedTextField(
                 value = cardNumber,
                 onValueChange = onCardNumberChange,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .focusable()
+                    .onFocusChanged {
+                        if (it.isFocused) keyboardController?.hide()
+                    }
+                    .clickable {
+                        focusRequester.requestFocus()
+                        keyboardController?.hide()
+                    },
                 singleLine = true,
                 textStyle = MaterialTheme.typography.bodyMedium.copy(color = CardDataTextColor),
                 shape = RoundedCornerShape(14.dp),
